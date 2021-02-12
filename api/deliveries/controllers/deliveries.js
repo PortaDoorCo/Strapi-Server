0860613bd2f6510ed2cc28ac724637185a8ba81a
heyjaypray <hey@jaypray.com>
2021-02-12 13:50:04 -0700

    update delivery api

--- api/deliveries/controllers/deliveries.js
+++ api/deliveries/controllers/deliveries.js
@@ -5,4 +5,17 @@
  * to customize this controller
  */
 
-module.exports = {};
+const { sanitizeEntity } = require('strapi-utils');
+
+module.exports = {
+    async find(ctx) {
+        let entities;
+        if (ctx.query._q) {
+          entities = await strapi.services.deliveries.search(ctx.query);
+        } else {
+          entities = await strapi.services.deliveries.find(ctx.query);
+        }
+
+        return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.deliveries }));
+      },
+};
