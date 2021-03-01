80746ad96c023e1941838c988af9d6c225bbcd3e
heyjaypray <hey@jaypray.com>
2021-03-01 09:13:57 -0700

    update api

--- api/printer-options/controllers/printer-options.js
+++ api/printer-options/controllers/printer-options.js
@@ -5,4 +5,23 @@
  * to customize this controller
  */
 
-module.exports = {};
+var _ = require('lodash');
+
+const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
+
+module.exports = {
+    async create(ctx) {
+        let entity;
+
+        const path  = _.kebabCase("printer_options")
+
+        if (ctx.is('multipart')) {
+          const { data, files } = parseMultipartData(ctx);
+          entity = await strapi.services[path].create(data, { files });
+        } else {
+          entity = await strapi.services[path].create(ctx.request.body);
+        }
+        strapi.emitToAllUsers("printer_option_added", ctx.params, entity);
+        return sanitizeEntity(entity, { model: strapi.models[path] });
+      },
+};
--- api/printer-options/services/printer-options.js
+++ api/printer-options/services/printer-options.js
@@ -4,5 +4,28 @@
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/services.html#core-services)
  * to customize this service
  */
+var _ = require('lodash');
 
-module.exports = {};
+
+module.exports = {
+    async create(data, { files } = {}) {
+        const path  = _.kebabCase("printer_options")
+        const validData = await strapi.entityValidator.validateEntityCreation(
+          strapi.models[path], 
+          data
+        );
+    
+        const entry = await strapi.query('printer-options').create(validData);
+
+        if (files) {
+          // automatically uploads the files based on the entry and the model
+          await strapi.entityService.uploadFiles(entry, files, {
+            model: 'printer-options',
+            // if you are using a plugin's model you will have to add the `source` key (source: 'users-permissions')
+          });
+          return this.findOne({ id: entry.id });
+        }
+
+        return entry;
+      },
+};
