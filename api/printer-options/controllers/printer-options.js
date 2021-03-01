4e9ef8f56f1fe7ffc007917a471ba00a1315f5dd
heyjaypray <hey@jaypray.com>
2021-03-01 09:54:48 -0700

    update api

--- api/printer-options/controllers/printer-options.js
+++ api/printer-options/controllers/printer-options.js
@@ -8,20 +8,33 @@
 var _ = require('lodash');
 
 const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
+const path  = _.kebabCase("printer_options")
 
 module.exports = {
     async create(ctx) {
         let entity;
-
-        const path  = _.kebabCase("printer_options")
-
         if (ctx.is('multipart')) {
           const { data, files } = parseMultipartData(ctx);
           entity = await strapi.services[path].create(data, { files });
         } else {
           entity = await strapi.services[path].create(ctx.request.body);
         }
-        strapi.emitToAllUsers("printer_option_added", ctx.params, entity);
+        strapi.emitToAllUsers("printer_option_added", entity);
+        return sanitizeEntity(entity, { model: strapi.models[path] });
+      },
+      async update(ctx) {
+        const { id } = ctx.params;
+        let entity;
+        if (ctx.is('multipart')) {
+          const { data, files } = parseMultipartData(ctx);
+          entity = await strapi.services[path].update({ id }, data, {
+            files,
+          });
+        } else {
+          entity = await strapi.services[path].update({ id }, ctx.request.body);
+        }
+
+        strapi.emitToAllUsers("printer_option_updated", ctx.params, entity);
         return sanitizeEntity(entity, { model: strapi.models[path] });
       },
 };
--- api/printer-options/services/printer-options.js
+++ api/printer-options/services/printer-options.js
@@ -1,31 +1,55 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/services.html#core-services)
  * to customize this service
  */
-var _ = require('lodash');
+var _ = require("lodash");
 
+const path = _.kebabCase("printer_options");
 
 module.exports = {
-    async create(data, { files } = {}) {
-        const path  = _.kebabCase("printer_options")
-        const validData = await strapi.entityValidator.validateEntityCreation(
-          strapi.models[path], 
-          data
-        );
+  async create(data, { files } = {}) {
     
-        const entry = await strapi.query('printer-options').create(validData);
+    const validData = await strapi.entityValidator.validateEntityCreation(
+      strapi.models[path],
+      data
+    );
 
-        if (files) {
-          // automatically uploads the files based on the entry and the model
-          await strapi.entityService.uploadFiles(entry, files, {
-            model: 'printer-options',
-            // if you are using a plugin's model you will have to add the `source` key (source: 'users-permissions')
-          });
-          return this.findOne({ id: entry.id });
-        }
+    const entry = await strapi.query("printer-options").create(validData);
 
-        return entry;
-      },
+    if (files) {
+      // automatically uploads the files based on the entry and the model
+      await strapi.entityService.uploadFiles(entry, files, {
+        model: "printer-options",
+        // if you are using a plugin's model you will have to add the `source` key (source: 'users-permissions')
+      });
+      return this.findOne({ id: entry.id });
+    }
+
+    return entry;
+  },
+  async update(params, data, { files } = {}) {
+    const existingEntry = await strapi.query('printer-options').findOne(params);
+
+
+    const validData = await strapi.entityValidator.validateEntityUpdate(
+      strapi.models[path], 
+      data
+    );
+
+    const entry = await strapi.query('printer-options').update(params, validData);
+
+    if (files) {
+      // automatically uploads the files based on the entry and the model
+      await strapi.entityService.uploadFiles(entry, files, {
+        model: 'printer-options',
+        // if you are using a plugin's model you will have to add the `source` key (source: 'users-permissions')
+      });
+      return this.findOne({ id: entry.id });
+    }
+
+    return entry;
+  },
+
 };
