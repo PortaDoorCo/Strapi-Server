de59d3d8d7921c1cc228c54ef1294244e0b52cba
heyjaypray <hey@jaypray.com>
2021-09-09 15:44:41 -0400

    add deployment notification

---
+++ api/deployments/controllers/deployments.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
+ * to customize this controller
+ */
+
+ const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
+
+module.exports = {
+    async create(ctx) {
+        strapi.emitToAllUsers("new_deployment", ctx.request.body);
+      },
+};
