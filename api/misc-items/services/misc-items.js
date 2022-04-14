fb519b56d441230e81709f299b0707e2bd7001de
Justin Romanos <hey@jaypray.com>
2022-04-14 14:13:26 -0700

    update misc items

--- api/misc-items/services/misc-items.js
+++ api/misc-items/services/misc-items.js
@@ -1,8 +1,10 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/services.html#core-services)
  * to customize this service
  */
+
+const { isDraft } = require("strapi-utils").contentTypes;
 
 module.exports = {};
