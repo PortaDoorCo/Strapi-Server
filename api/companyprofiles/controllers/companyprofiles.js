b151aff28de9d0e1590bec2bfcb5e3a3102d7293
heyjaypray <hey@jaypray.com>
2022-06-13 07:34:00 -0400

    update customer response

--- api/companyprofiles/controllers/companyprofiles.js
+++ api/companyprofiles/controllers/companyprofiles.js
@@ -25,7 +25,8 @@ module.exports = {
     // Send 201 `created`
     ctx.created(data);
     // NEW LINE: call our method emitToAllUsers and pass it body request
-    strapi.emitToAllUsers("customer_added", customer);
+    console.log({ data });
+    strapi.emitToAllUsers("customer_added", data);
   },
   update: async (ctx) => {
     let entity;
@@ -44,8 +45,8 @@ module.exports = {
       );
     }
 
-    console.log({entry})
-    console.log({entity})
+    console.log({ entry });
+    console.log({ entity });
 
     strapi.emitToAllUsers("customer_updated", entity);
     return sanitizeEntity(entity, { model: strapi.models.orders });
@@ -58,17 +59,25 @@ module.exports = {
       entities = await strapi.query("companyprofiles").find(ctx.query);
     }
 
-    return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.companyprofiles }));
+    return entities.map((entity) =>
+      sanitizeEntity(entity, { model: strapi.models.companyprofiles })
+    );
   },
   async findAll(ctx) {
     let entities;
     if (ctx.query._q) {
-      entities = await strapi.query("companyprofiles").find({_limit: 2500, _sort: 'CUSTNO:ASC'});
+      entities = await strapi
+        .query("companyprofiles")
+        .find({ _limit: 2500, _sort: "CUSTNO:ASC" });
     } else {
-      entities = await strapi.query("companyprofiles").find({_limit: 2500, _sort: 'CUSTNO:ASC'});
+      entities = await strapi
+        .query("companyprofiles")
+        .find({ _limit: 2500, _sort: "CUSTNO:ASC" });
     }
 
-    return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.companyprofiles }));
+    return entities.map((entity) =>
+      sanitizeEntity(entity, { model: strapi.models.companyprofiles })
+    );
   },
   async findOne(ctx) {
     const { id } = ctx.params;
@@ -80,9 +89,8 @@ module.exports = {
     const { id } = ctx.params;
 
     const entity = await strapi.services.companyprofiles.delete({ id });
-    console.log({entity})
+    console.log({ entity });
     strapi.emitToAllUsers("customer_deleted", entity);
     return sanitizeEntity(entity, { model: strapi.models.companyprofiles });
- 
   },
 };
