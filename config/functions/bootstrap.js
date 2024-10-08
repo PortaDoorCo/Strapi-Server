77e096b6d3e1e96d8d6df780e4f27f9138c4a3ef
Justin <hey@jaypray.com>
2024-10-08 15:52:45 -0700

    update

--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -1,26 +1,38 @@
 "use strict";
 
-/**
- * An asynchronous bootstrap function that runs before
- * your application gets started.
- *
- * This gives you an opportunity to set up your data model,
- * run jobs, or perform some special logic.
- *
- * See more details here: https://strapi.io/documentation/developer-docs/latest/concepts/configurations.html#bootstrap
- */
-
 module.exports = async () => {
-  process.nextTick(() => {
+  process.nextTick(async () => {
     const io = require("socket.io")(strapi.server, {
       cors: {
         origin: "*",
         methods: ["GET", "POST"],
       },
     });
-    const redis = require("socket.io-redis");
+
+    const { createClient } = require("redis");
     const redisAdapter = require("socket.io-redis");
-    io.adapter(redisAdapter(process.env.REDIS_URL));
+
+    // Create Redis clients with TLS options
+    const pubClient = createClient({
+      url: process.env.REDIS_URL,
+      socket: {
+        tls: true,
+        rejectUnauthorized: false,
+      },
+    });
+
+    const subClient = pubClient.duplicate();
+
+    // Handle errors
+    pubClient.on("error", (err) => console.error("Redis Client Error", err));
+    subClient.on("error", (err) => console.error("Redis Client Error", err));
+
+    // Connect the clients
+    await pubClient.connect();
+    await subClient.connect();
+
+    // Use the clients in the adapter
+    io.adapter(redisAdapter({ pubClient, subClient }));
 
     const users = [];
     let drivers = [];
@@ -28,15 +40,10 @@ module.exports = async () => {
     console.log("users", users);
 
     const getDrivers = () => {
-      let clients = io.sockets.clients().connected;
-      // console.log({ client: clients?.client });
-      let sockets = Object.values(clients);
-      let drivers = sockets.map((s) => {
-        // console.log({ s });
-        return s.driver;
-      });
-      // console.log({ drivers });
-      return drivers.filter((i) => i);
+      let clients = io.sockets.sockets;
+      let sockets = Array.from(clients.values());
+      let drivers = sockets.map((s) => s.driver).filter(Boolean);
+      return drivers;
     };
 
     const emitDrivers = () => {
@@ -44,28 +51,24 @@ module.exports = async () => {
     };
 
     io.on("connection", (socket) => {
-      // console.log({ socket });
-      // console.log(socket.id);
       socket.user_id = Math.random() * 100000000000000; // not so secure
       users.push(socket); // save the socket to use it later
 
       socket.on("position", (position) => {
-        // console.log("position", position);
         socket.driver = position;
-        console.log("hiiii");
         emitDrivers();
       });
 
       socket.on("disconnect", () => {
         emitDrivers();
         users.forEach((user, i) => {
-          // delete saved user when they disconnect
           if (user.user_id === socket.user_id) users.splice(i, 1);
         });
       });
     });
+
     strapi.io = io;
-    // send to all users connected
+    // Send to all users connected
     strapi.emitToAllUsers = (status, order, updatedStatus) =>
       io.emit(status, order, updatedStatus);
   });
