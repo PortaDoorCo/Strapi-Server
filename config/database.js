dc65c192b556f17d9243a1749f3f4a7e2ba2400f
Justin <hey@jaypray.com>
2024-10-08 15:23:28 -0700

    update

--- config/database.js
+++ config/database.js
@@ -32,11 +32,13 @@ module.exports = ({ env }) => ({
         username: env("DATABASE_USERNAME"),
         password: env("DATABASE_PASSWORD"),
         schema: env("DATABASE_SCHEMA", "public"),
-        // ssl: {
-        //   rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates },
-        // },
+        ssl: {
+          rejectUnauthorized: false,
+        },
       },
-      options: {},
+      options: {
+        ssl: true,
+      },
     },
   },
 });
